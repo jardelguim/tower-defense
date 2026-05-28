0d0c097df58d55a6a42917ae680ee6c1c9c35e88
Jardel <jardelguim23@gmail.com>
2026-05-28 14:01:13 -0300

    new tower type

--- src/entities/tower.gd
+++ src/entities/tower.gd
@@ -1,13 +1,15 @@
-class_name Tower extends StaticBody3D
+extends StaticBody3D
+class_name Tower 
 
 @export var data: TowerData
 
+@export var model : MeshInstance3D
+@export var fire_point: Marker3D
 @onready var range_area: Area3D = $RangeArea
-@onready var attack_timer: Timer = $AttackTimer
-@onready var fire_point: Marker3D = $FirePoint
+@onready var attack_timer = Timer.new()
 
 func _ready() -> void:
-	pass
+	_setup_timer()
 
 func _process(_delta: float) -> void:
 	pass
@@ -16,4 +18,13 @@ func _select_target() -> Node3D:
 	return null
 
 func _fire() -> void:
+	print("%s shoot!" %data.display_name)
+
+func _setup_timer():
+	add_child(attack_timer)
+	attack_timer.connect("timeout" , _fire)
+	attack_timer.wait_time = data.attack_speed
+	attack_timer.start()
+
+func _setup_stats():
 	pass
--- src/resources/TowerData.gd
+++ src/resources/TowerData.gd
@@ -1,9 +1,10 @@
-class_name TowerData extends Resource
+extends Resource
+class_name TowerData
 
 @export var display_name: String = "Basic Tower"
 @export var damage: float = 10.0
-@export_range(5.0, 30.0) var range: float = 15.0
 @export var attack_speed: float = 1.0
+@export_range(5.0, 30.0) var range: float = 15.0
 @export var cost: int = 50
 @export var scene: PackedScene
 @export var bullet_scene: PackedScene
